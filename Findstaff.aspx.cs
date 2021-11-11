386977f66f48d3bec28ad45350331d580d5e3dbb
mbitu <mbitumutonga@gmail.com>
2021-11-11 17:15:16 +0300

    C# methods

---
+++ Findstaff.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace First
+{
+    public partial class Findstaff : System.Web.UI.Page
+    {
+        //declare global variables
+        string username;
+        int mnumber;
+        bool status;
+        private bool findstaff(string email, string title, int staffId)
+        {
+            if(email=="joe@gmail.com"&& title=="Dr"&& staffId==1)
+            {
+                username = "Joe Joe";
+                mnumber = 12;
+                status = true;
+            }
+            else if(email=="phil@gmail.com"&& title=="Mr"&& staffId==2)
+            {
+                username = "Phil Phil";
+                mnumber = 13;
+                status = true;
+            }
+            else
+            {
+                status = false;
+            }
+            return status;
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            //call method
+            status = findstaff(txtStaffEmail.Text, txtStaffTitle.Text, int.Parse(txtStaffId.Text));
+            if (status == true)
+            {
+                lblDisplay.Text = "User Name:\t" + username + "Mobile Number:\t" + mnumber;
+                lblDisplay.ForeColor = System.Drawing.Color.Green;
+            }
+            else if(status==false)
+            {
+                lblDisplay.Text = "No search result matches the three parameters";
+                lblDisplay.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+    }
+}
---
+++ Method1.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace First
+{
+    public partial class Method1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            loadData("Joe", "joe@gmail.com", 12345, 1001);
+        }
+        private void loadData(string sname,string email,int mobile,int regno)
+        {
+            txtEmail.Text = email;
+            txtMobilenumber.Text = mobile.ToString();
+            txtRegnumber.Text = regno.ToString();
+            txtSname.Text = sname;
+            //calDob.SelectedDate = "1990-06-07";
+            
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            //Clear dta from the textbox
+        }
+        private void ClearData()
+        {
+            txtEmail.Text = "";
+            txtMobilenumber.Text = "";
+            txtRegnumber.Text = "";
+            txtSname.Text = "";
+        }
+
+        protected void btnLoadData_Click(object sender, EventArgs e)
+        {
+            //call load data method
+            loadData("Joe", "joe@gmail.com", 12345, 1001);
+        }
+    }
+}
